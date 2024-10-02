d661c4c6b49bea4dfa764a5b10a721c2245b2b76
MrMiu <alexanderjruan@gmail.com>
2024-10-01 21:49:13 -0400

    skeleton code for malloc()

--- mymalloc.c
+++ mymalloc.c
@@ -17,7 +17,7 @@ static union {
 } heap;
 
 void *mymalloc(size_t size, char *file, int line) {
-  size = (size + 7) & ~7;
+  int newSize = (size + 7) & ~7;
 
   // Access two values of starting header
   int isUsed = *(int*) heap.bytes;
@@ -29,31 +29,40 @@ void *mymalloc(size_t size, char *file, int line) {
   // Used to update isUsed and sizeOfPayload while header jumping
   int tempSizeOfPayload = 0;
 
+  // Check to see if there is a header next to the header selected
+  int avaliableHeader = 0;
+
   // Header jumping: continue going to headers until free header
   // TODO: CHECK TO ENSURE NO INDEX-OUT-BOUNDS WHEN HEADER JUMPING TO THE END
-  while ((isUsed != 0) || (sizeOfPayload >= size)) {
+  while ((isUsed != 0) || (sizeOfPayload >= newSize)) {
     tempSizeOfPayload = sizeOfPayload;
-    indexOfHeader = 8 + sizeOfPayload;
+    indexOfHeader = HEADER_SIZE + sizeOfPayload;
 
-    isUsed = (*(int*) heap.bytes) + 8 + sizeOfPayload;
+    isUsed = (*(int*) heap.bytes) + HEADER_SIZE + sizeOfPayload;
     sizeOfPayload = (*(int*) heap.bytes) + 12 + tempSizeOfPayload;
   }
 
-  // Update current header values
-  int *isUsedPtr = heap.bytes + indexOfHeader;
-  int *sizeOfPayloadPtr = heap.bytes + indexOfHeader + 4;
-  *isUsedPtr = 1;
-  *sizeOfPayloadPtr = size; 
-
-  // Checking to ensure if creation of header is needed
-  // TODO: Code logic is wrong. Need to ensure there is no header at sizeOfChunkandLocation
-  int sizeOfChunkandLocation = indexOfHeader + 8 + sizeOfPayload;
-  if (MEMLENGTH - (sizeOfChunkandLocation) != 0) {
-    // Point these pointers to supposed header in heap
-    isUsedPtr = heap.bytes + indexOfHeader + *sizeOfPayloadPtr;
-    sizeOfPayloadPtr = heap.bytes + indexOfHeader + 4 + *sizeOfPayloadPtr;
+  if ((indexOfHeader + HEADER_SIZE + sizeOfPayload) < MEMLENGTH) {
+	avaliableHeader = 1;
   }
 
+  // Update current header values
+  int *isUsedPtr = (int*) heap.bytes + indexOfHeader;
+  int *sizeOfPayloadPtr = (int*) heap.bytes + indexOfHeader + 4;
+  *isUsedPtr = 1;
+  *sizeOfPayloadPtr = newSize; 
+
+  // Check to ensure creation of header is needed
+  if (avaliableHeader == 0) {
+    // Point these pointers to supposed header in heap
+    int *nextIsUsedPtr = (int*) heap.bytes + indexOfHeader + *sizeOfPayloadPtr;
+    int *nextSizeOfPayloadPtr = (int*) heap.bytes + indexOfHeader + 4 + *sizeOfPayloadPtr;
+
+	// Create values for new header
+	*nextIsUsedPtr = 0;
+	*nextSizeOfPayloadPtr = MEMLENGTH - (indexOfHeader + 4 + *sizeOfPayloadPtr);
+  }
+  return isUsedPtr;
 }
 
 static int initialized = 0;
